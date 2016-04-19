f712377b7e8b481f03528fa749fe6a34770896c4
Alexandre BERDERY <alexandre.berdery@provenrun.com>
2016-04-19 14:09:58 +0200

    Add support for test launching in qemu
    If a 2nd file is given in command line, it will be loaded as a json file
    containing informations about test suite to run:
    - domains: list of tests domain used to build/link tests into final image
    - tests: for each domain, list of tests with possible args and optional
    timeout value

--- code_sitter.py
+++ code_sitter.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
-import sys, os, json
+import sys, os, json, traceback
 from datetime import datetime
 from subprocess import Popen, PIPE
 
 from code_sitter_cmd import build_recipe_C, build_recipe_SM, subcommand
 
-def main(config_file):
+def main(config_file, test_file=None):
     current_path = os.getcwd()
+    tests=None
+
     try:
         fp = open(config_file)
         jsconfig = json.load(fp)
+        fp.close()
     except Exception as e:
         print "Unable to read configuration file '%s': %s\n"%(config_file, str(e))
         sys.exit(-1)
+
+    if test_file != None:
+        try:
+            fp = open(test_file)
+            tests = json.load(fp)
+            fp.close()
+        except Exception as e:
+            print "Unable to read test suite file '%s': %s\n"%(test_file, str(e))
+            sys.exit(-1)
+
     try:
         config = jsconfig['config']
         projects = jsconfig['projects']
@@ -25,20 +38,20 @@ def main(config_file):
         now = datetime.now()
         print "\n *** Compilation Test: %s ***\n"%(str(now))
 
-        print "Cleaning old build data:"
-        for project in projects:
-            name = project['name']
-            subcommand("Deleting %s"%name, ['rm', '-rf', name], current_path, "  ")
+        if repo_path != 'none':
+            print "Cleaning old build data:"
+            for project in projects:
+                name = project['name']
+                subcommand("Deleting %s"%name, ['rm', '-rf', name], current_path, "  ")
 
-        print "\nCloning projects:"
-        for project in projects:
-            name = project['name']
-            hgpath = os.path.join(repo_path, name)
-            prjpath = os.path.join(current_path, name)
-            print "Project %s:"%name
-            subcommand("Cloning %s ..."%name, ['hg', 'clone', hgpath], current_path, "  ")
-            subcommand("Updating %s ..."%name, ['hg', 'pull'], prjpath, "  ")
-
+            print "\nCloning projects:"
+            for project in projects:
+                name = project['name']
+                hgpath = os.path.join(repo_path, name)
+                prjpath = os.path.join(current_path, name)
+                print "Project %s:"%name
+                subcommand("Cloning %s ..."%name, ['hg', 'clone', hgpath], current_path, "  ")
+                subcommand("Updating %s ..."%name, ['hg', 'pull'], prjpath, "  ")
 
         print "\nBuilding recipes:"
         for project in projects:
@@ -55,32 +68,38 @@ def main(config_file):
             for branch in branches:
                 branch_name = branch['branch']
                 targets = branch['targets']
-                subcommand("Reseting %s to branch %s ..."%(name, branch_name),
-                        ['hg', 'up', branch_name], prjpath, "  ")
+                if repo_path != 'none':
+                    subcommand("Reseting %s to branch %s ..."%(name, branch_name),
+                            ['hg', 'up', branch_name], prjpath, "  ")
                 for target in targets:
                     target_name = target['target']
                     run_qemu = target['qemu']
                     if recipe == 'c':
                         build_recipe_C(current_path, name, "%s_config"%target_name,
-                                config, run_qemu, "  ")
+                                config, run_qemu, "  ", tests)
                     elif recipe == 'smart':
                         build_recipe_SM(current_path, name, "%s_config"%target_name,
-                                config, run_qemu, "  ")
+                                config, run_qemu, "  ", tests)
                     else:
                         print "Unknown recipe '%s', skipping..."%recipe
-        print "\n\n *** All builds are successful ***\n"
+        print "\n\n *** All recipes are successful ***\n"
 
-        print "Final cleaning:"
-        for project in projects:
-            name = project['name']
-            subcommand("Deleting %s"%name, ['rm', '-rf', name], current_path, "  ")
+        if repo_path != 'none':
+            print "Final cleaning:"
+            for project in projects:
+                name = project['name']
+                subcommand("Deleting %s"%name, ['rm', '-rf', name], current_path, "  ")
+
         sys.exit(0)
-    except Exception as e:
-        print "Invalid configuration file '%s': %s\n"%(config_file, str(e))
+    except Exception:
+        type, value, history = sys.exc_info()
+        traceback.print_exception(type, value, history, 10)
         sys.exit(-1)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
+        main(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) > 1:
         main(sys.argv[1])
     else:
         main('config.json')
--- code_sitter_cmd.py
+++ code_sitter_cmd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, os, threading, json
+import sys, os, threading, json, pexpect
 from subprocess import Popen, PIPE
 
 class RunCmd(threading.Thread):
@@ -23,7 +23,13 @@ class RunCmd(threading.Thread):
             print "RunCmd output:\n%s\n"%self.p.stdout.read()
             self.join()
 
-def subcommand(cmd, cmdlist, path, prefix):
+def which(file):
+    for path in os.environ["PATH"].split(os.pathsep):
+        if os.path.exists(os.path.join(path, file)):
+            return os.path.join(path, file)
+    return None
+
+def subcommand(cmd, cmdlist, path, prefix, display=True):
     proc = Popen(cmdlist, stdout=PIPE, cwd=path)
     cmd_output = ""
     while proc.poll() is None:
@@ -34,37 +40,136 @@ def subcommand(cmd, cmdlist, path, prefix):
         print "%sFull log:\n%s\n"%(prefix, cmd_output)
         print "%sCommand '%s' failed with error code %d"%(prefix, cmd, proc.returncode)
         sys.exit(-1)
-    print "%sCommand completed successfully: %s"%(prefix, cmd)
+    if display == True:
+        print "%sCommand completed successfully: %s"%(prefix, cmd)
 
+def runTests(tests, path, prefix, emu_bin=None, emu_args=None):
+    # Run all tests one by one
+    # For each test get end infos
+    # Compute results
+    prefix = '  ' + prefix
+    num_tests=0
+    num_pass=0
+    num_fail=0
+    num_inc=0
+    num_notrun=0
+    #default timeout: 20s
+    timeout = 20
 
-def build_recipe(path, name, target, config, run_qemu, prefix):
-    full_path = os.path.join(path, name)
+    f=open(os.path.join('default_tests_log.txt'), 'w')
+    for domain in tests['domains']:
+        for test in domain['tests']:
+            num_tests+=1
+            tcmd = test['name']
+            if test['args'] != 'none':
+                tcmd += ' ' + test['args']
+            t = timeout
+            try:
+                t = test['timeout']
+            except:
+                pass
+            p = pexpect.spawn(emu_bin, emu_args, cwd=path, logfile=f)
+            p.expect('ProvenCore\[', timeout=t)
+            p.sendline(tcmd)
+            idx = p.expect(['<--- PNC TEST STATUS:', 'Command failed', pexpect.TIMEOUT], timeout=t)
+            if idx == 1:
+                print "%sTest %s: NOT RUN" % (prefix, test['name'])
+                num_notrun+=1
+            elif idx == 2:
+                print "%sTest %s: TIMEOUT" % (prefix, test['name'])
+                num_fail+=1
+            else:
+                idx=p.expect(['PASS', 'FAIL', 'INCONCLUSIVE'])
+                if idx == 0:
+                    print "%sTest %s: PASS" % (prefix, test['name'])
+                    num_pass+=1
+                elif idx == 1:
+                    print "%sTest %s: FAIL" % (prefix, test['name'])
+                    num_fail+=1
+                elif idx == 2:
+                    print "%sTest %s: INCONCLUSIVE" % (prefix, test['name'])
+                    num_inc+=1
+
+    f.close()
+    return num_tests, num_pass, num_fail, num_inc, num_notrun
+
+def configTests(tests, path, prefix=None):
+    # Update config.mk to build init with only shell
+    afile = os.path.join(path, 'config.mk')
+    f = open(afile, 'a')
+    f.write('FEATURES += INIT_SHELL\n')
+    f.close()
+    print "%s  config.mk updated successfully."%prefix
+
+    # Create pnc_tests.mk containing all test domains to build/link in image
+    tfile = os.path.join(path, 'pnc_tests.mk')
+    subcommand("rm -f %s"%tfile, ['rm', '-f', tfile], path, "%s  "%prefix, display=False)
+    f = open(tfile, 'w')
+    f.write('PNC_TESTS := \\\n')
+    for domain in tests['domains']:
+        f.write('  '+domain['name']+'\\\n')
+    f.close()
+    print "%s  pnc_tests.mk created successfully."%prefix
+
+def setup_toolchain(prefix):
+    # check for <prefix>-gcc as direct path or in os.environ["PATH"]
+    gcc = prefix + 'gcc'
+    if not os.path.exists(gcc):
+        path = which(gcc)
+        if not path:
+            toolchain = None
+        else:
+            toolchain = os.path.join(os.path.dirname(path), prefix)
+    else:
+        toolchain = prefix
+    return toolchain
+
+def build_recipe(path, name, target, config, run_qemu, prefix, tests=None):
+    # Check if cross compile var is available
     try:
-        qemu_path = config['qemu-path']
-        qemu_bin = os.path.join(qemu_path, config['qemu-bin'])
-        qemu_cmd = [qemu_bin] + config['qemu-args'].split(' ')
+        cross_compile = "CROSS_COMPILE="
+        toolchain = setup_toolchain(config['toolchain'])
+        if toolchain != None:
+            cross_compile = "CROSS_COMPILE=%s"%toolchain
     except Exception as e:
-        print "Configuration issue: can't create qemu command: %s"%str(e)
-        sys.exit(-1)
+        pass
+
     print "%sBuilding %s:%s:"%(prefix, name,target)
+    full_path = os.path.join(path, name)
     subcommand("make mrproper", ['make', 'mrproper'], full_path, "%s  "%prefix)
     subcommand("make distclean", ['make', 'distclean'], full_path, "%s  "%prefix)
-    subcommand("make %s"%target, ['make', target], full_path, "%s  "%prefix)
-    subcommand("make config", ['make', 'config'], full_path, "%s  "%prefix)
-    subcommand("make programs", ['make', 'programs'], full_path, "%s  "%prefix)
-    subcommand("make", ['make', 'all'], full_path, "%s  "%prefix)
-    if run_qemu:
-        RunCmd(qemu_cmd, PIPE, full_path, 5).Run()
+    subcommand("make %s %s"%(target, cross_compile), ['make', target, cross_compile], full_path, "%s  "%prefix)
+    if tests != None:
+        configTests(tests, full_path, prefix)
+    subcommand("make config %s"%cross_compile, ['make', 'config', cross_compile], full_path, "%s  "%prefix)
+    subcommand("make programs %s"%cross_compile, ['make', 'programs', cross_compile], full_path, "%s  "%prefix)
+    subcommand("make %s"%cross_compile, ['make', 'all', cross_compile], full_path, "%s  "%prefix)
     print "%sBuilding %s:%s is a success.\n"%(prefix, name,target)
+
+    # run qemu with/without tests
+    if run_qemu:
+        try:
+            qemu_path = config['qemu-path']
+            qemu_bin = os.path.join(qemu_path, config['qemu-bin'])
+            qemu_cmd = [qemu_bin] + config['qemu-args'].split(' ')
+        except Exception as e:
+            print "Configuration issue: can't create qemu command: %s"%str(e)
+            sys.exit(-1)
+        if tests == None:
+            RunCmd(qemu_cmd, PIPE, full_path, 5).Run()
+        else:
+            print "%sRunning tests for %s:%s"%(prefix, name,target)
+            (n, p, f, i, nr)=runTests(tests, full_path, prefix, emu_bin=qemu_bin, emu_args=config['qemu-args'].split(' '))
+            print "%sSUMMARY: %d tests, %d PASS, %d FAIL, %d INCONCLUSIVE, %d NOT RUN" % (prefix, n, p, f, i, nr)
 
 def setup_recipe(path, name, prefix):
     full_path = os.path.join(path, name)
     print "%sPreparing %s/IMP."%(prefix, name)
     subcommand("sh setup_kernel.sh", ['sh', 'setup_kernel.sh'], full_path, "%s  "%prefix)
 
-def build_recipe_C(path, name, target, config, run_qemu, prefix):
-    build_recipe(path, name, target, config, run_qemu, prefix)
+def build_recipe_C(path, name, target, config, run_qemu, prefix, tests=None):
+    build_recipe(path, name, target, config, run_qemu, prefix, tests)
 
-def build_recipe_SM(path, name, target, config, run_qemu, prefix):
+def build_recipe_SM(path, name, target, config, run_qemu, prefix, tests=None):
     setup_recipe(path, name, prefix)
-    build_recipe(os.path.join(path, name), "IMP", target, config, run_qemu, prefix)
+    build_recipe(os.path.join(path, name), "IMP", target, config, run_qemu, prefix, tests)
